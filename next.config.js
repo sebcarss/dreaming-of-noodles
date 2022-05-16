d5ec2be8bf949586bafad7c79aba6f8e13badea9
Seb Carss <seb.carss@gmail.com>
2022-05-16 20:39:32 +0100

    Change generateBuildId as netlify logs suggest

--- next.config.js
+++ next.config.js
@@ -2,7 +2,5 @@ module.exports = {
     images: {
         domains: ['dreamingofnoodles.s3.eu-west-1.amazonaws.com'],
     },
-    generateBuildId: async () => {
-        return 'my-build-id'
-    },
+    generateBuildId: () => 'build',
 }
