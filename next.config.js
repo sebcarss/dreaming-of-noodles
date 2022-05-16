35dd0b2e6c94210ddd51eee22d72db4a34fe5335
Seb Carss <seb.carss@gmail.com>
2022-05-16 20:36:16 +0100

    Generate build ID as git hash

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     images: {
         domains: ['dreamingofnoodles.s3.eu-west-1.amazonaws.com'],
-    }
+    },
+    generateBuildId: async () => {
+        return 'my-build-id'
+    },
 }
