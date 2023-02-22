efec08123d49c509720d4f615aa62d6c33c1e632
Seb Carss <seb.carss@gmail.com>
2023-02-22 22:05:00 +0000

    Add white full width separator for prefectures

--- components/region-link-grid.tsx
+++ components/region-link-grid.tsx
@@ -33,9 +33,21 @@ export default function RegionLinkGrid({ linkData, heading }: RegionLinkGridProp
   });
 
   return (
-    <div>
+    <div style={{
+      paddingBottom: "10px",
+      width: "100vw",
+      left: "50%",
+      right: "50%",
+      marginLeft: "-50vw",
+      marginRight: "-50vw",
+      position: "relative",
+      backgroundColor: "white",
+      maxWidth: "100vw",
+      borderTop: "1px black solid",
+      borderBottom: "1px black solid"
+    }}>
       <Row className="mt-3">
-          <h2 style={{ textAlign: "center" }}>{heading}</h2>
+        <h2 style={{ textAlign: "center" }}>{heading}</h2>
       </Row>
       <Row xs={2} sm={4} lg={6} className="g-1 mt-1">
         {links}
