4534830be858cc5eff93a1a7fbc4a5abd1e34232
Seb Carss <seb.carss@gmail.com>
2023-05-13 16:33:36 +0100

    Fixed prefecture tests

--- pages-tests/japan/prefecture.test.tsx
+++ pages-tests/japan/prefecture.test.tsx
@@ -1,6 +1,7 @@
 import Prefecture from "../../pages/japan/[prefecture]";
 import renderer from "react-test-renderer";
 import { LinkCardData } from "../../types/LinkCardData";
+import { PostData } from '../../types/PostData';
 
 const regionData: LinkCardData = {
     frontmatter: {
@@ -12,9 +13,21 @@ const regionData: LinkCardData = {
     content: "\nWelcome to Hokkaido!"
 };
 
+const posts: PostData[] = [
+    {
+        title: "Hello World 2",
+        id: "2019-01-02-hello-world-2",
+        date: "2019-01-02",
+        tags: ["hello", "world"],
+        published: true,
+        excerpt: 'Hello excerpt',
+        image: "https://source.unsplash.com/random/300x200",
+    }
+];
+
 describe("Prefecture page", () => {
     it('should match the snapshot', () => {
-        const tree = renderer.create(<Prefecture regionData={regionData} />).toJSON();
+        const tree = renderer.create(<Prefecture regionData={regionData} posts={posts} />).toJSON();
         expect(tree).toMatchSnapshot();
     });
 });
